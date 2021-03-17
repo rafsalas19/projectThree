767413692bf337a195c066ca829c49f3857ba358
rafsalas19 <rafsalas1987@hotmail.com>
2021-03-17 00:54:10 +0000

    add xv6 code with null page and frame for protect calls

---
+++ xv6-null-pages/src/nullPtest.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/time.h>
+// #include <assert.h>
+// #include <string.h>
+
+int main(int argc, char *argv[])
+{
+  int * testPoint =0;
+  
+  printf(1,"This is my NULL pointer address: %p\n", testPoint);
+  printf(1,"This is my NULL pointer address: %d\n", *testPoint);
+//return 0;
+  exit();
+}
